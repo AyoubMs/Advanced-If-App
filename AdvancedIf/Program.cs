45aa91d45303fa8301d6a88de28a7a7bdc6a46fb
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:19:49 +0200

    add app

---
+++ AdvancedIf/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+
+if (firstName.ToLower().Equals("tim") && lastName.ToLower().Equals("corey"))
+{
+    Console.WriteLine("Hello Mr. Corey");
+}
